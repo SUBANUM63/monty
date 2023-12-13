fda970bc2d3964b50bfd844d20e122cfc5e86cc0
Zeinu Kedir <zeinuka641@gmail.com>
2023-12-13 11:26:27 -0800

    swaps the top two elements of the stack

--- swap.c
+++ swap.c
@@ -1,6 +1,6 @@
 #include "monty.h"
 /**
- * f_swap - swaps the top two elements of the stack.
+ * f_swap - swaps the top two elements of the stack
  * @head: stack head
  * @counter: line_number
  * Return: no return
